853a27bd74f2785ba9591babb856bc0bb178db27
ArketLaguageCenter <96335533+ArketLaguageCenter@users.noreply.github.com>
2021-12-18 20:47:43 +0630

    Add files via upload

--- certificate/script.js
+++ certificate/script.js
@@ -3,7 +3,9 @@ const ctx = canvas.getContext('2d')
 
 const nameInput = document.getElementById('name')
 const nrcInput = document.getElementById('nrc')
+const classInput = document.getElementById('class')
 const dateInput = document.getElementById('date')
+const cidInput = document.getElementById('cid')
 
 const downloadBtn = document.getElementById('download-btn')
 
@@ -20,7 +22,9 @@ function drawImage() {
 	ctx.fillStyle = '#29e'
 	ctx.fillText(nameInput.value, 430, 200)
         ctx.fillText(nrcInput.value, 350, 250)
+        ctx.fillText(classInput.value, 350, 360)
         ctx.fillText(dateInput.value, 190, 560)
+        ctx.fillText(cidInput.value, 500, 660)
 }
 
 
@@ -31,4 +35,5 @@ nameInput.addEventListener('input', function () {
 downloadBtn.addEventListener('click', function () {
 	downloadBtn.href = canvas.toDataURL('image/jpg')
 	downloadBtn.download = 'Certificate - ' + nameInput.value
+        downloadBtn.download = 'Certificate - ' + nrcInput.value
 })
