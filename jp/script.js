0dbd5213643894414e1a82c063efe576691f835e
ArketLaguageCenter <96335533+ArketLaguageCenter@users.noreply.github.com>
2021-12-25 10:48:24 +0630

    Update script.js

--- jp/script.js
+++ jp/script.js
@@ -32,9 +32,19 @@ function drawImage() {
 //---CID---//
         ctx.fillText(cidInput.value, 500, 660)
 }
-
-
 nameInput.addEventListener('input', function () {
+	drawImage()
+})
+nrcInput.addEventListener('input', function () {
+	drawImage()
+})
+classInput.addEventListener('input', function () {
+	drawImage()
+})
+dateInput.addEventListener('input', function () {
+	drawImage()
+})
+cidInput.addEventListener('input', function () {
 	drawImage()
 })
 
