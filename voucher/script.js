06696411ff3cf0b2e7ab2cdb20054d770a3e753f
ielcmm <96643772+ielcmm@users.noreply.github.com>
2021-12-25 12:26:29 +0630

    Update script.js

--- voucher/script.js
+++ voucher/script.js
@@ -4,11 +4,13 @@ const ctx = canvas.getContext('2d')
 const nameInput = document.getElementById('name')
 const nrcInput = document.getElementById('nrc')
 const classInput = document.getElementById('class')
-//---OUTPUT---//
+const dateInput = document.getElementById('date')
+const cidInput = document.getElementById('cid')
+
 const downloadBtn = document.getElementById('download-btn')
 
 const image = new Image()
-image.src = 'IELC Certificate.jpg'
+image.src = 'certificate.jpg'
 image.onload = function () {
 	drawImage()
 }
@@ -16,24 +18,34 @@ image.onload = function () {
 function drawImage() {
 	// ctx.clearRect(0, 0, canvas.width, canvas.height)
 	ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
-	ctx.font = '35px Georgia'
-	ctx.fillStyle = '#0f4070'
+	ctx.font = '17px Gadugi bold'
+	ctx.fillStyle = '#000000'
 
 //---NAME---//
-	ctx.fillText(nameInput.value, 270, 285)
+	ctx.fillText(nameInput.value, 95, 240)
 //---NAME---//
-	ctx.fillText(nrcInput.value, 270, 325)
+	ctx.fillText(nrcInput.value, 40, 350)
 //---CLASS---//
-        ctx.fillText(classInput.value, 270, 394)
+        ctx.fillText(classInput.value, 380, 350)
+        ctx.fillText(classInput.value, 380, 400)
+//---DATE---//
+        ctx.fillText(dateInput.value, 360, 240)
 }
+
 nameInput.addEventListener('input', function () {
-	drawImage()})
+	drawImage()
+})
 nrcInput.addEventListener('input', function () {
-	drawImage()})
+	drawImage()
+})
 classInput.addEventListener('input', function () {
-	drawImage()})
+	drawImage()
+})
+dateInput.addEventListener('input', function () {
+	drawImage()
+})
 
 downloadBtn.addEventListener('click', function () {
 	downloadBtn.href = canvas.toDataURL('image/jpg')
-	downloadBtn.download = 'IELC' + nameInput.value
+	downloadBtn.download = 'voucher' + nameInput.value
 })
